0c6cfd344d60e7550e1988f44038308d00ae6b29
mmonirul <mohammadmonirul@gmail.com>
2024-11-27 01:29:04 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly. Hello world</li>
+          <li>Save and see your changes instantly. Hello world again.</li>
         </ol>
 
         <div className='flex gap-4 items-center flex-col sm:flex-row'>
